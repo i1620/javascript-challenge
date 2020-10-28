8a0bf3c33d506f382ec93be22c4f8e24e05c92e8
i1620 <68018020+i1620@users.noreply.github.com>
2020-10-28 18:40:11 -0500

    made changes

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,23 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(tableData);
+
+// Reference to table body
+var tbody = d3.select("tbody");
+
+// Values for each column
+tableData.forEach(function (sightingData){
+    console.log(sightingData);
+    
+    var row = tbody.append("tr");
+
+    // https://masteringjs.io/tutorials/fundamentals/foreach-object
+    // Using 'Object.entries'
+    Object.defineProperties(sightingData).forEach(function ([key, value]) {
+        console.log(key, value);
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
