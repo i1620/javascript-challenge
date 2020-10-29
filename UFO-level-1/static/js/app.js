1354a5e41b8b10e9d100da1461af4e63a9328f1c
i1620 <68018020+i1620@users.noreply.github.com>
2020-10-28 19:09:14 -0500

    finished app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,21 +4,54 @@ var tableData = data;
 // YOUR CODE HERE!
 console.log(tableData);
 
-// Reference to table body
+// Use D3 to select the table body
 var tbody = d3.select("tbody");
 
 // Values for each column
-tableData.forEach(function (sightingData){
+tableData.forEach(function (sightingData) {
     console.log(sightingData);
     
+    // Append one table row `tr` to the table body
     var row = tbody.append("tr");
 
     // https://masteringjs.io/tutorials/fundamentals/foreach-object
     // Using 'Object.entries'
-    Object.defineProperties(sightingData).forEach(function ([key, value]) {
+    Object.entries(sightingData).forEach(function ([key, value]) {
         console.log(key, value);
 
         var cell = row.append("td");
         cell.text(value);
+    });
+});
+
+// Select the Button
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    
+    tbody.html("");
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue)
+    console.log(filteredData);
+
+    filteredData.forEach(function (selection) {
+        console.log(selection);
+
+        // Append one table row `tr` to the table body
+        var row = tbody.append("tr");
+
+        // Using 'Object.entries'
+        Object.entries(selection).forEach(function ([key, value]) {
+            console.log(key, value);
+
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
 });
